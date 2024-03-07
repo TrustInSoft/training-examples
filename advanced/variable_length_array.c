96061000898be9c7d84d888d0b1aa3673e9d6a03
Clément Gillard <clement.gillard@trust-in-soft.com>
2024-03-07 13:44:31 +0100

    VLA: use separate function

--- advanced/variable_length_array.c
+++ advanced/variable_length_array.c
@@ -1,9 +1,12 @@
 // tis-analyzer -val -val-profile interpreter variable_length_array.c
 
-int main() {
-    int size = 42;
+void f(int size) {
     short v[size];
     int sizeof_v = sizeof v;
     for (int i = 0; i < size; ++i)
       v[i] = i;
+}
+
+int main() {
+    f(42);
 }
