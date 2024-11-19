fbd6c3d64dce9c3539418082115638a4ebaf82a1
Clément Gillard <clement.gillard@trust-in-soft.com>
2024-11-19 10:44:14 +0100

    genstr3.c: Use macros instead of magic values
    Use `limits.h`'s `CHAR_MIN` and `CHAR_MAX` instead of hardcoded
    values.

--- level2/5_generalized_strings/genstr3.c
+++ level2/5_generalized_strings/genstr3.c
@@ -1,13 +1,14 @@
 // tis-analyzer -val -val-profile analyzer genstr3.c
 // How to produce a properly generalized string with no unexpected '\0'?
 
+#include <limits.h>
 #include <string.h>
 #include <tis_builtin.h>
 
 int main(void) {
     char buffer[101];
     for (size_t i = 0; i < sizeof buffer - 1; i++) {
-        buffer[i] = tis_nondet(tis_interval(-128, -1), tis_interval(1, 127));
+        buffer[i] = tis_nondet(tis_interval(CHAR_MIN, -1), tis_interval(1, CHAR_MAX));
     }
     buffer[sizeof buffer - 1] = 0;
     size_t len = strlen(buffer); // We want 100 and only 100
